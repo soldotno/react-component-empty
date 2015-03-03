534e32a388a28765e8aa1c3cbccee6215d35c570
misund <thomas@hemmeligadresse.com>
2015-03-03 14:51:58 +0100

    Taking React as a dependency.

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 'use strict';
-var React = require('react');
 
-module.exports = React.createClass({
-    displayName: 'EmptyComponent',
+module.exports = function(React) {
+    return React.createClass({
+        displayName: 'EmptyComponent',
 
-    getInitialState: function () {
-        return null;
-    },
+        getInitialState: function () {
+            return null;
+        },
 
-    shouldComponentUpdate: function() {
-        return false;
-    },
+        shouldComponentUpdate: function() {
+            return false;
+        },
 
-    render: function() {
-        return null;
-    }
-});
+        render: function() {
+            return null;
+        }
+    });
+}
